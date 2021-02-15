ebff224ec0f4481621cb4e13a60f58de836de512
antirez <antirez@gmail.com>
2021-02-15 11:01:33 +0100

    SQLite abstraction: life is too short.

--- stonky.c
+++ stonky.c
@@ -137,10 +137,10 @@ void xfree(void *ptr) {
 #define JSEL_TYPECHECK 3      /* ":" */
 #define JSEL_MAX_TOKEN 256
 cJSON *cJSON_Select(cJSON *o, const char *fmt, ...) {
-    va_list ap;
     int next = JSEL_INVALID;        /* Type of the next selector. */
     char token[JSEL_MAX_TOKEN+1];   /* Current token. */
     int tlen;                       /* Current length of the token. */
+    va_list ap;
 
     va_start(ap,fmt);
     const char *p = fmt;
@@ -226,6 +226,195 @@ cleanup:
 notfound:
     o = NULL;
     goto cleanup;
+}
+
+/* ============================================================================
+ * SQLite abstraction
+ * ==========================================================================*/
+#define SQL_MAX_SPEC 32     /* Maximum number of ?... specifiers per query. */
+
+/* The sqlCol and sqlRow structures are used in order to return rows. */
+typedef struct sqlCol {
+    int type;
+    int64_t i;          /* Integer or len of string/blob. */
+    const char *s;      /* String or blob. */
+    double d;           /* Double. */
+} sqlCol;
+
+typedef struct sqlRow {
+    sqlite3_stmt *stmt; /* Handle for this query. */
+    int cols;           /* Number of columns. */
+    sqlCol *col;        /* Array of columns. Note that the first time this
+                           will be NULL, so we now we don't need to call
+                           sqlite3_step() since it was called by the
+                           query function. */
+} sqlRow;
+
+/* This is the low level function that we use to model all the higher level
+ * functions. It is based on the idea that dbHandle is a per-thread SQLite
+ * handle already available: the rest of the code will ensure this.
+ *
+ * Queries can contain ?s ?i and ?d special specifiers that are bound to
+ * the SQL query, and must be present later as additional arguments after
+ * the 'sql' argument.
+ *
+ * The function returns the return code of the last SQLite query that
+ * failed on error. On success it returns what sqlite3_step() returns.
+ * If the function returns SQLITE_ROW, that is, if the query is
+ * returning data, the function returns, by reference, a sqlRow object
+ * that the caller can use to get the current and next rows.
+ *
+ * The user needs to later free this sqlRow object with sqlEnd() (but this
+ * is done automatically if all the rows are consumed with sqlNextRow()).
+ * Note that is valid to call sqlEnd() even if the query didn't return
+ * SQLITE_ROW, since in such case row->stmt is set to NULL.
+ */
+int sqlGenericQuery(sqlRow *row, const char *sql, va_list ap) {
+    int rc = SQLITE_ERROR;
+    sqlite3_stmt *stmt = NULL;
+    sds query = sdsempty();
+
+    /* We need to build the query, substituting the following three
+     * classes of patterns with just "?", remembering the order and
+     * type, and later using the sql3 binding API in order to prepare
+     * the query:
+     *
+     * ?s string
+     * ?i int64_t
+     * ?d double
+     */
+    char spec[SQL_MAX_SPEC];
+    int numspec = 0;
+    const char *p = sql;
+    while(p[0]) {
+        if (p[0] == '?') {
+            if (p[1] == 's' || p[1] == 'i' || p[2] == 'd') {
+                if (numspec == SQL_MAX_SPEC) goto error;
+                spec[numspec++] = p[1];
+            } else {
+                goto error;
+            }
+            query = sdscatlen(query,"?",1);
+        } else {
+            query = sdscatlen(query,p,1);
+        }
+        p++;
+    }
+
+    /* Prepare the query and bind the query arguments. */
+    rc = sqlite3_prepare_v2(dbHandle,query,-1,&stmt,NULL);
+    if (rc != SQLITE_OK) goto error;
+
+    for (int j = 0; j < numspec; j++) {
+        switch(spec[j]) {
+        case 's': rc = sqlite3_bind_text(stmt,j+1,va_arg(ap,char*),-1,NULL);
+                  break;
+        case 'i': rc = sqlite3_bind_int64(stmt,j+1,va_arg(ap,int64_t));
+                  break;
+        case 'd': rc = sqlite3_bind_double(stmt,j+1,va_arg(ap,double));
+                  break;
+        }
+        if (rc != SQLITE_OK) goto error;
+    }
+
+    /* Execute. */
+    rc = sqlite3_step(stmt);
+    if (rc == SQLITE_ROW) {
+        if (row) {
+            row->stmt = stmt;
+            row->cols = 0;
+            row->col = NULL;
+            stmt = NULL; /* Don't free it on cleanup. */
+        }
+    } else {
+        if (row) row->stmt = NULL;
+    }
+
+error:
+    if (stmt) sqlite3_finalize(stmt);
+    sdsfree(query);
+    return rc;
+}
+
+/* This function should be called only if you don't get all the rows
+ * till the end. It is safe to call anyway. */
+void sqlEnd(sqlRow *row) {
+    xfree(row->col);
+    if (row->stmt) sqlite3_finalize(row->stmt);
+    row->col = NULL;
+    row->stmt = NULL;
+}
+
+/* After sqlGenericQuery() returns SQLITE_ROW, you can call this function
+ * with the 'row' object pointer in order to get the rows composing the
+ * result set. It returns 1 if the next row is available, otherwise 0
+ * is returned (and the row object is freed). If you stop the iteration
+ * before all the elements are used, you need to call sqlEnd(). */
+int sqlNextRow(sqlRow *row) {
+    if (row->col != NULL) {
+        if (sqlite3_step(row->stmt) != SQLITE_ROW) {
+            sqlEnd(row);
+            return 0;
+        }
+    }
+
+    xfree(row->col);
+    row->cols = sqlite3_data_count(row->stmt);
+    row->col = xmalloc(sizeof(row->cols)*sizeof(sqlCol));
+    for (int j = 0; j < row->cols; j++) {
+        row->col[j].type = sqlite3_column_type(row->stmt,j);
+        if (row->col[j].type == SQLITE_INTEGER) {
+            row->col[j].i = sqlite3_column_int64(row->stmt,j);
+        } else if (row->col[j].type == SQLITE_FLOAT) {
+            row->col[j].d = sqlite3_column_double(row->stmt,j);
+        } else if (row->col[j].type == SQLITE_TEXT) {
+            row->col[j].s = (char*)sqlite3_column_text(row->stmt,j);
+            row->col[j].i = sqlite3_column_bytes(row->stmt,j);
+        } else if (row->col[j].type == SQLITE_BLOB) {
+            row->col[j].s = sqlite3_column_blob(row->stmt,j);
+            row->col[j].i = sqlite3_column_bytes(row->stmt,j);
+        } else {
+            /* SQLITE_NULL. */
+            row->col[j].s = NULL;
+            row->col[j].i = 0;
+            row->col[j].d = 0;
+        }
+    }
+    return 1;
+}
+
+/* Wrapper for sqlGenericQuery() returning the last inserted ID or 0
+ * on error. */
+int sqlInsert(const char *sql, ...) {
+    int64_t lastid = 0;
+    va_list ap;
+    va_start(ap,sql);
+    int rc = sqlGenericQuery(NULL,sql,ap);
+    if (rc == SQLITE_DONE) lastid = sqlite3_last_insert_rowid(dbHandle);
+    va_end(ap);
+    return lastid;
+}
+
+/* Wrapper for sqlGenericQuery() returning 1 if the query resulted in
+ * SQLITE_DONE, otherwise zero. This is good for UPDATE and DELETE
+ * statements. */
+int sqlQuery(const char *sql, ...) {
+    int64_t retval = 0;
+    va_list ap;
+    va_start(ap,sql);
+    int rc = sqlGenericQuery(NULL,sql,ap);
+    retval = (rc == SQLITE_DONE);
+    va_end(ap);
+    return retval;
+}
+/* Wrapper for sqlGenericQuery() using varialbe number of args.
+ * This is what you want when doing SELECT queries. */
+int sqlSelect(sqlRow *row, const char *sql, ...) {
+    va_list ap;
+    va_start(ap,sql);
+    int rc = sqlGenericQuery(row,sql,ap);
+    va_end(ap);
+    return rc;
 }
 
 /* ============================================================================
@@ -1807,9 +1996,18 @@ void *scanStocksThread(void *arg) {
             printf("evenbetter: %d/%d %s\n",j,NumSymbols,symbol);
             dbAddStockToList("evenbetter", symbol);
             showstats=1;
+        } else if (mcvl.gain > 0.5 && mcvl.absdiffper < 100 &&
+                   mclong.gain > 0.5 && mclong.absdiffper < 100 &&
+                   mcshort.gain > 0.5 && mcshort.absdiffper < 100 &&
+                   mcvs.gain > 0.5 && mcvs.absdiffper < 100)
+        {
+            printf("consistency: %d/%d %s\n",j,NumSymbols,symbol);
+            dbAddStockToList("consistency", symbol);
+            showstats=1;
         } else {
             dbDelStockFromList("tothemoon", symbol, 0);
             dbDelStockFromList("evenbetter", symbol, 0);
+            dbDelStockFromList("consistency", symbol, 0);
         }
 
         if (showstats) {
@@ -1828,7 +2026,8 @@ void *scanStocksThread(void *arg) {
                 mcday.mingain, mcday.maxgain);
         }
 
-        sleep(1);
+        //sleep(1);
+        usleep(100000);
     }
     dbClose();
     return NULL;
